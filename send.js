7932ad0bf2b6f93bb506b6dcf261861e318f9ebc
Cornbread2100 <cornbread2100@cornbread2100.com>
2024-01-25 14:13:22 -0500

    try/catch varint

--- send.js
+++ send.js
@@ -3,39 +3,41 @@ const varint = require('varint');
 
 module.exports = (ip, port, packet, timeout = 5000) => {
   return new Promise(async (resolve, reject) => {
-    try {
-      var data = Buffer.alloc(0);
-      var length;
-      var hasResponded = false;
+    var data = Buffer.alloc(0);
+    var length;
+    var hasResponded = false;
 
-      setTimeout(() => {
-        if (!hasResponded) {
-          client.destroy();
-          resolve('timeout');
-        }
-      }, timeout)
-
-      const client = new net.Socket();
-      client.connect(port, ip, () => client.write(packet));
-      client.on('error', err => {
+    setTimeout(() => {
+      if (!hasResponded) {
         client.destroy();
-        resolve(err.toString());
-      });
-      client.on('data', newData => {
-        if (data.length == 0) {
+        resolve('timeout');
+      }
+    }, timeout)
+
+    const client = new net.Socket();
+    client.connect(port, ip, () => client.write(packet));
+    client.on('error', err => {
+      client.destroy();
+      resolve(err.toString());
+    });
+    client.on('data', newData => {
+      if (data.length == 0) {
+        try {
           length = varint.decode(newData);
           newData = newData.subarray(varint.decode.bytes);
-        }
-        data = Buffer.concat([data, newData]);
-        if (data.length >= length) {
-          data = data.subarray(0, length);
-          hasResponded = true;
+        } else {
+          length = 0;
+          newData = Buffer.alloc(0);
           client.destroy();
-          resolve(data);
         }
-      })
-    } catch (error) {
-      console.log('error');
-    }
+      }
+      data = Buffer.concat([data, newData]);
+      if (data.length >= length) {
+        data = data.subarray(0, length);
+        hasResponded = true;
+        client.destroy();
+        resolve(data);
+      }
+    })
   })
 }
