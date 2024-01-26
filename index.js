9b886f61f7c81589341fef6656e814163d8e5d45
Cornbread2100 <cornbread2100@cornbread2100.com>
2024-01-25 22:24:43 -0500

    save players.sample as an array

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function main() {
             ip: server.ip,
             port: server.port,
             version: response.version,
-            players: { online: response.players.online, max: response.players.max },
+            players: response.players,
             description: response.description,
             enforcesSecureChat: response.enforcesSecureChat,
             hasFavicon: response.favicon != null,
@@ -108,12 +108,7 @@ async function main() {
         }
 
         const update = { $set: newObj };
-        if (Symbol.iterator in Object(response.players?.sample)) {
-          for (player of response.players.sample) {
-            update.$set.players[`sample.${player.name.replaceAll('.', '')}:${player.id}`] = lastSeen;
-            update.$set.players[`history.${player.name}:${player.uuid}`] = lastSeen;
-          }
-        }
+        if (Symbol.iterator in Object(response.players?.sample)) for (player of response.players.sample) update.$set.players[`history.${player.name}:${player.uuid}`] = lastSeen;
         operations.push({
           updateOne: {
             filter: { ip: server.ip, port: server.port },
