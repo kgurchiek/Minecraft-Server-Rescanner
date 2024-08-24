4efc27708125ea2b5a5d994ccbe22ee6f68026e8
Cornbread2100 <cornbread2100@cornbread2100.com>
2024-08-23 20:40:59 -0400

    fix auth check

--- ping.js
+++ ping.js
@@ -42,50 +42,48 @@ module.exports = {
       }
     })
   },
-  authCheck: (ip, port, protocol, timeout) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const mcData = minecraftData(protocol);
-        const username = 'discord.gg/3u2fNRAMAN'; // server invite in case concerned server owners want to contact me
-    
-        const handshakePacket = Buffer.concat([
-          Buffer.from([0x00]), // packet ID
-          Buffer.from(varint.encode(protocol)), //protocol version
-          Buffer.from([ip.length]),
-          Buffer.from(ip, 'utf-8'), // server address
-          Buffer.from(new Uint16Array([port]).buffer).reverse(), // server port
-          Buffer.from([0x02]) // next state (2)
-        ]);
-        var packetLength = Buffer.alloc(1);
-        packetLength.writeUInt8(handshakePacket.length);
-        var buffer = Buffer.concat([packetLength, handshakePacket]);
-    
-        const packetFormat = mcData.protocol.login.toServer.types.packet_login_start[1];
-        var buffers = [Buffer.from([0x00])];
-        for (var i = 0; i < packetFormat.length; i++) {
-          if (packetFormat[i].type.includes('option')) {
-            buffers.push(Buffer.from([0x00]));
-          } else {
-            switch (packetFormat[i].name) {
-              case 'username':
-                buffers.push(Buffer.from([username.length])); // length of username
-                buffers.push(Buffer.from(username, 'utf-8')); // username
-                break;
-            }
+  authCheck: async (ip, port, protocol, timeout) => {
+    try {
+      const mcData = minecraftData(protocol);
+      const username = `Cornbread2100${Math.floor(Math.random() * 100)}`;
+      var hasResponded = false;
+
+      const handshakePacket = Buffer.concat([
+        Buffer.from([0x00]), // packet ID
+        Buffer.from(varint.encode(protocol)), //protocol version
+        Buffer.from([ip.length]),
+        Buffer.from(ip, 'utf-8'), // server address
+        Buffer.from(new Uint16Array([port]).buffer).reverse(), // server port
+        Buffer.from([0x02]) // next state (2)
+      ]);
+      var packetLength = Buffer.alloc(1);
+      packetLength.writeUInt8(handshakePacket.length);
+      var buffer = Buffer.concat([packetLength, handshakePacket]);
+
+      const packetFormat = mcData.protocol.login.toServer.types.packet_login_start[1];
+      var buffers = [Buffer.from([0x00])];
+      for (var i = 0; i < packetFormat.length; i++) {
+        if (packetFormat[i].type.includes('option')) {
+          buffers.push(Buffer.from([0x00]));
+        } else {
+          switch (packetFormat[i].name) {
+            case 'username':
+              buffers.push(Buffer.from([username.length])); // length of username
+              buffers.push(Buffer.from(username, 'utf-8')); // username
+              break;
+            default:
+              break;
           }
         }
-  
-        const startLoginPacket = Buffer.concat(buffers);
-        packetLength = Buffer.alloc(1);
-        packetLength.writeUInt8(startLoginPacket.length);
-        buffer = Buffer.concat([buffer, packetLength, startLoginPacket]);
-        var response = await send(ip, port, buffer, timeout);
-        if (typeof response == 'string') resolve(response);
-        else if (response[0] == 0) resolve('unknown');
-        else resolve(response[0] == 1);
-      } catch (error) {
-        resolve('error')
       }
-    });
+
+      const startLoginPacket = Buffer.concat(buffers);
+      packetLength = Buffer.alloc(1);
+      packetLength.writeUInt8(startLoginPacket.length);
+      const response = await send(ip, port, Buffer.concat([buffer, packetLength, startLoginPacket]), 6000);
+      if (typeof response != 'string' && response[0] == 0) console.log(response.toString())
+      if (typeof response == 'string') return `Error: ${response}`;
+      else return response[0] == 0 ? 'unknown' : (response[0] != 1);
+    } catch (error) { return `Error: ${error}`; }
   }
 }
